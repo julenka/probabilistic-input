ea7b09babcc28d78f5f16ba3956165361ec6fd4d
julenka <julenka@cs.cmu.edu>
2014-03-02 21:59:58 -0500

    Adding inheritance, unit tests, refactoring out color methods.

--- experiments/keyboard_pf/particle_filter.js
+++ experiments/keyboard_pf/particle_filter.js
@@ -327,3 +327,55 @@ ParticleFilter.prototype.aggregate = function() {
     }
     this.reducedParticles =this.reducedParticles.sort(function(a,b) { return b.weight - a.weight;});
 };
+
+
+function rgb(r,g,b) {
+    var result = {
+        'r': r,
+        'g': g,
+        'b': b,
+        'a': 1.0
+    };
+    return result;
+}
+
+function rgba(r,g,b,a) {
+    var result = {
+        'r': r,
+        'g': g,
+        'b': b,
+        'a': a,
+    };
+    return result;
+}
+
+function hsvToRgb(h,s,v) {
+    var r, g, b, i, f, p, q, t;
+    if (h && s === undefined && v === undefined) {
+        s = h.s;
+        v = h.v;
+        h = h.h;
+    }
+    i = Math.floor(h * 6);
+    f = h * 6 - i;
+    p = v * (1 - s);
+    q = v * (1 - f * s);
+    t = v * (1 - (1 - f) * s);
+    switch (i % 6) {
+        case 0: r = v; g = t; b = p; break;
+        case 1: r = q; g = v; b = p; break;
+        case 2: r = p; g = v; b = t; break;
+        case 3: r = p; g = q; b = v; break;
+        case 4: r = t; g = p; b = v; break;
+        case 5: r = v; g = p; b = q; break;
+    }
+    return {
+        r: Math.floor(r * 255),
+        g: Math.floor(g * 255),
+        b: Math.floor(b * 255),
+        a: 1.0
+    };
+}
+function getFillStyle(c) {
+    return "rgba("+c.r+","+c.g+","+c.b+","+c.a+")";
+}
--- experiments/libs/utils.js
+++ experiments/libs/utils.js
@@ -6,8 +6,61 @@ if(!jQuery) {
     throw{name:"FatalError", message:"using utils.js, but jQuery is not loaded! Did you forget to load jQuery?"};
 }
 
+// Inheritance
+// Obtained from "Secrets of the JavaScript Ninja", page 145
+(function(){
+    var initializing = false,
+        superPattern =
+            /xyz/.test(function() {xyz;}) ? /\b_super\b/ : /.*/; // determines in functions can be serialized
 
+    Object.subClass = function(properties) {
+        var _super = this.prototype;
 
+        // instantiate the super class
+        initializing = true;
+        var proto = new this();
+        initializing = false;
+
+        for(var name in properties) {
+            proto[name] =
+                typeof properties[name] == "function" && // make sure the property being overriden is a function
+                typeof _super[name] == "function" && // make sure that the superclass property is also a function
+                superPattern.test(properties[name]) ? // and the function that we are copying calls _super
+                (function(name, fn) {
+                    return function() {
+                        // save ths pointer to the super class
+                        var tmp = this._super;
+
+                        // set the new super class to be the superclass's super class
+                        this._super = _super[name];
+
+                        // call the super class function. Remember that arguments is a special param
+                        var ret = fn.apply(this, arguments);
+
+                        this._super = tmp;
+
+                        return ret;
+                    };
+                })(name, properties[name]):
+                properties[name];
+        }
+
+        function Class() {
+            // if we are not initializing, that is, setting up the prototype for extension
+            // AND the init method is defined (it should always be defined)
+            // call the init method
+            if(!initializing && this.init) {
+                this.init.apply(this, arguments);
+            }
+        }
+
+        Class.prototype = proto;
+        Class.constructor = Class;
+        Class.subClass = arguments.callee;
+
+        return Class;
+    };
+})();
 // extracts a random sample from list of weighted samples
 // samples are in the form:
 // { 
@@ -15,7 +68,7 @@ if(!jQuery) {
 //    value_2: weight_2,
 // }
 // assumes that the weights all sum to 1
-Math.weighted_random_sample(map) {
+Math.weighted_random_sample = function(map) {
     var r = Math.random();
     var sum = 0;
     var last = undefined;
@@ -55,7 +108,6 @@ Math.gaussian = function(mu, sigma, x) {
 };
 
 // Augmenting Array
-
 Array.prototype.pop_front = function() {
     var rslt = this.splice(0,1);
     return rslt[0];
@@ -69,62 +121,10 @@ Array.prototype.min = function() {
   return Math.min.apply(null, this);
 };
 
-function rgb(r,g,b) {
-    var result = {
-        'r': r,
-        'g': g,
-        'b': b,
-        'a': 1.0
-    };
-    return result;
-}
-
-function rgba(r,g,b,a) {
-    var result = {
-        'r': r,
-        'g': g,
-        'b': b,
-        'a': a,
-    };
-    return result;
-}
-
-function hsvToRgb(h,s,v) {
-    var r, g, b, i, f, p, q, t;
-    if (h && s === undefined && v === undefined) {
-        s = h.s; 
-        v = h.v;
-        h = h.h;
-    }
-    i = Math.floor(h * 6);
-    f = h * 6 - i;
-    p = v * (1 - s);
-    q = v * (1 - f * s);
-    t = v * (1 - (1 - f) * s);
-    switch (i % 6) {
-        case 0: r = v; g = t; b = p; break;
-        case 1: r = q; g = v; b = p; break;
-        case 2: r = p; g = v; b = t; break;
-        case 3: r = p; g = q; b = v; break;
-        case 4: r = t; g = p; b = v; break;
-        case 5: r = v; g = p; b = q; break;
-    }
-    return {
-        r: Math.floor(r * 255),
-        g: Math.floor(g * 255),
-        b: Math.floor(b * 255),
-        a: 1.0
-    };
-}
-
 function remap(v, i_min, i_max, o_min, o_max) {
     if(v < i_min) v = i_min;
     if (v > i_max) v = i_max;
     return (v - i_min) / (i_max - i_min) * (o_max - o_min) + o_min;
-}
-
-function getFillStyle(c) {
-    return "rgba("+c.r+","+c.g+","+c.b+","+c.a+")";
 }
 
 // Logging
