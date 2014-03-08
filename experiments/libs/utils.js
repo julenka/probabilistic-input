ff6c21d51b54bcc4f8116c870bfe1d28502ebf69
julenka <julenka@cs.cmu.edu>
2014-03-08 17:59:20 -0500

    Adding curry to Function prototype (with test)

--- experiments/libs/utils.js
+++ experiments/libs/utils.js
@@ -181,7 +181,13 @@ Array.prototype.shuffle = function() {
     }
 };
 
-
+Function.prototype.curry = function() {
+    var fn = this,
+        args = Array.prototype.slice.call(arguments);
+    return function() {
+        return fn.apply(this, args.concat(
+            Array.prototype.slice.call(arguments)));
+    }; };
 
 // Logging
 function Logger(level) {
