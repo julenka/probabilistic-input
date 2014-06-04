66bd0dbde8f919c46514304c5c061b73008d813b
julenka <julenka@cs.cmu.edu>
2014-06-04 18:52:35 -0400

    FormEntry migrate to using properties.

--- julia/views/form_entry.js
+++ julia/views/form_entry.js
@@ -1,37 +1,70 @@
 /**
  * Created by julenka on 6/4/14.
  */
+var FormLabel = View.subClass({
+    className: "FormLabel",
+    init: function(julia, props) {
+        this.super(julia, props, {text: "empty", x: 0, y: 0});
+    },
+    draw: function($el) {
+        var s = Snap($el[0]);
+        s.text(this.properties.x, this.properties.y, this.properties.label);
+    }
+});
 
 var FormEntry = FSMView.subClass({
     // TODO: Migrate this to using properties when you feel like doing mindless refactoring
-    className: "FormLabel",
-    init: function(julia, label, inputValidFunction, entryTextValidFunction, x, y, onCompleteFn) {
-        this._super(julia);
-        this.entry_text = "";
-        this.entry_opacity = 0.6;
-        this.x = x;
-        this.y = y;
-        this.width = 400;
-        this.height = 30;
-        this.label = label;
-        this.message = "";
-        this.onCompleteFn = onCompleteFn;
-        this.validFunction = inputValidFunction;
-        this.entryTextValidFunction = entryTextValidFunction;
-        this.interim_text = "";
+    className: "FormEntry",
+    isDeleteKey: function(keyCode) {
+        return String.fromCharCode(keyCode).toLowerCase() === "x";
+    },
+    isTabOrEnter: function(keyCode) {
+        return keyCode === 9 || keyCode === 13;
+    },
+    updateMessage: function(message, rootView) {
+//        rootView.findViewById(this.properties.label_id).text = "keep going...";
+        this.properties.message = message;
+    },
+
+    /**
+     *
+     * @param julia
+     * @param label
+     * @param inputValidFunction
+     * @param entryTextValidFunction
+     * @param x
+     * @param y
+     * @param onCompleteFn
+     * message_id will be id of the message view
+     * label_id will be id of the label view
+     */
+    init: function(julia, props, inputValidFunction, entryTextValidFunction, entryCompleted) {
+        var defaults = {
+            entry_text: "",
+            entry_opacity: 0.6,
+            x: 0,
+            y: 0,
+            width: 400,
+            height: 30,
+            interim_text: "",
+            label: "no label",
+            message: "",
+            entryCompleted: entryCompleted
+        };
+        this._super(julia, props, defaults);
         this.fsm_description = {
             start: [
                 new KeypressTransition(
                     "textEntered",
                     function(e) {
-                        return !(e.keyCode == 9 || e.keyCode == 13 || String.fromCharCode(e.keyCode).toLowerCase() === "x") &&
-                            this.validFunction(String.fromCharCode(e.keyCode));
+                        return !(this.isTabOrEnter(e.keyCode) ||  this.isDeleteKey(e.keyCode)) &&
+                            inputValidFunction(String.fromCharCode(e.keyCode));
                     },
-                    function(e) {
-                        this.entry_opacity = 0.6;
-                        this.entry_text += String.fromCharCode(e.keyCode);
+                    function(e, rootView) {
+                        this.properties.entry_opacity = 0.6;
+                        this.properties.entry_text += String.fromCharCode(e.keyCode);
                         this.container.setFocus(this);
-                        this.message = "keep going...";
+                        this.updateMessage("keep going...", rootView);
                     }, // feedback
                     undefined,
                     true
@@ -39,12 +72,12 @@ var FormEntry = FSMView.subClass({
                 new VoiceTransition(
                     "voiceEntered",
                     "interim",
-                    function(e) { return this.validFunction(e.transcript); },
+                    function(e) { return inputValidFunction(e.transcript); },
                     function(e) {
-                        this.entry_opacity = 0.6;
-                        this.entry_text = e.transcript;
+                        this.properties.entry_opacity = 0.6;
+                        this.properties.entry_text = e.transcript;
                         this.container.setFocus(this);
-                        this.message = "keep going...";
+                        this.properties.message = "keep going...";
                     },
                     undefined,
                     true
@@ -53,31 +86,28 @@ var FormEntry = FSMView.subClass({
                 new VoiceTransition(
                     "start",
                     "interim",
-                    function(e) { return true },
-                    function(e) {},
+                    function() { return true },
+                    function() {},
                     undefined,
                     true
-                    // to,source,type,predicate,feedback_action,final_action,handles_event)
                 )
             ],
             voiceEntered: [
                 new VoiceTransition(
                     "voiceEntered",
                     "interim",
-                    function(e) { return this.validFunction(e.transcript); },
-                    function(e) {
-                        this.entry_opacity = 0.6;
-
-                        this.message = "keep going...";
+                    function(e) { return inputValidFunction(e.transcript); },
+                    function(e, rootView) {
+                        this.properties.entry_opacity = 0.6;
+                        this.updateMessage("keep going...", rootView);
                     },
                     undefined,
                     true
-                    // to,source,type,predicate,feedback_action,final_action,handles_event)
                 ),
                 new VoiceTransition(
                     "done",
                     "final",
-                    function(e) { return this.entryTextValidFunction(e.transcript); },
+                    function(e) { return entryTextValidFunction(e.transcript); },
                     undefined,
                     this.entry_completed,
                     true
@@ -86,7 +116,7 @@ var FormEntry = FSMView.subClass({
                 new MouseDownTransition(
                     "done",
                     function(e) {
-                        return this.predicate_mouse_in_region(e) && this.entryTextValidFunction(this.entry_text);
+                        return this.predicate_mouse_in_region(e) && entryTextValidFunction(this.properties.entry_text);
                     },
                     undefined,
                     this.entry_completed,
@@ -95,10 +125,10 @@ var FormEntry = FSMView.subClass({
                 new MouseDownTransition(
                     "voiceEntered",
                     function(e) {
-                        return this.predicate_mouse_in_region(e) && !this.entryTextValidFunction(this.entry_text);
+                        return this.predicate_mouse_in_region(e) && !entryTextValidFunction(this.properties.entry_text);
                     },
                     function() {
-                        this.message = "not valid!"
+                        this.properties.message = "not valid!"
                     },
                     undefined,
                     false
@@ -111,7 +141,7 @@ var FormEntry = FSMView.subClass({
                 new MouseDownTransition(
                     "done",
                     function(e) {
-                        return this.predicate_mouse_in_region(e) && this.entryTextValidFunction(this.entry_text);
+                        return this.predicate_mouse_in_region(e) && entryTextValidFunction(this.properties.entry_text);
                     },
                     undefined,
                     this.entry_completed,
@@ -120,17 +150,19 @@ var FormEntry = FSMView.subClass({
                 new MouseDownTransition(
                     "textEntered",
                     function(e) {
-                        return this.predicate_mouse_in_region(e) && !this.entryTextValidFunction(this.entry_text);
+                        return this.predicate_mouse_in_region(e) && !entryTextValidFunction(this.properties.entry_text);
                     },
-                    function() {
-                        this.message = "not valid!"
+                    function(rootView) {
+                        this.updateMessage("not valid!", rootView);
                     },
                     undefined,
                     false
                 ),
                 new KeypressTransition(
                     "done",
-                    function(e, rootView) { return e.keyCode == 13 && this.entryTextValidFunction(this.entry_text); }, // 9 is tab, 13 is enter
+                    function(e, rootView) {
+                        return e.keyCode === 13 &&
+                            entryTextValidFunction(this.properties.entry_text); }, // 9 is tab, 13 is enter
                     undefined,
                     this.entry_completed, // final
                     true
@@ -138,9 +170,10 @@ var FormEntry = FSMView.subClass({
             /** Pressing the enter key but entry is invalid **/
                 new KeypressTransition(
                     "textEntered",
-                    function(e) { return e.keyCode == 13 && !this.entryTextValidFunction(this.entry_text); }, // 9 is tab, 13 is enter
-                    function() {
-                        this.message = "not valid!"
+                    function(e) { return e.keyCode === 13 &&
+                        !entryTextValidFunction(this.properties.entry_text); }, // 9 is tab, 13 is enter
+                    function(e, rootView) {
+                        this.updateMessage("not valid!", rootView);
                     },
                     undefined, // final
                     true
@@ -149,12 +182,12 @@ var FormEntry = FSMView.subClass({
                 new KeypressTransition(
                     "start",
                     function(e) {
-                        return String.fromCharCode(e.keyCode).toLowerCase() === "x" && this.entry_text.length <= 1;
+                        return this.isDeleteKey(e.keyCode) && this.properties.entry_text.length <= 1;
                     },
-                    function(e) {
-                        this.entry_text = "";
+                    function(e, rootView) {
+                        this.properties.entry_text = "";
                         this.container.clearFocus();
-                        this.message = "";
+                        this.updateMessage("", rootView);
                     },
                     undefined,
                     true
@@ -163,19 +196,19 @@ var FormEntry = FSMView.subClass({
                 new KeypressTransition(
                     "textEntered",
                     function(e) {
-                        return !(e.keyCode == 9 || e.keyCode == 13) &&
-                            !(String.fromCharCode(e.keyCode).toLowerCase() === "x" && this.entry_text.length <= 1);
+                        return !this.isTabOrEnter(e.keyCode) &&
+                            !(this.isDeleteKey(e.keyCode) && this.properties.entry_text.length <= 1);
                     },
                     function(e, rootView) {
-                        if(!(this.validFunction(String.fromCharCode(e.keyCode)))) {
+                        if(!(inputValidFunction(String.fromCharCode(e.keyCode)))) {
                             rootView.kill = true;
                             return;
                         }
-                        if(String.fromCharCode(e.keyCode).toLowerCase() === "x") {
-                            this.entry_text = this.entry_text.substring(0, this.entry_text.length - 2);
+                        if(this.isDeleteKey(e.keyCode)) {
+                            this.properties.entry_text = this.properties.entry_text.substring(0, this.properties.entry_text.length - 2);
                         } else {
-                            this.entry_text += String.fromCharCode(e.keyCode);
-                            this.message = "keep going...";
+                            this.properties.entry_text += String.fromCharCode(e.keyCode);
+                            this.updateMessage("keep going...", rootView);
                         }
 
 
@@ -190,49 +223,38 @@ var FormEntry = FSMView.subClass({
         }
     },
     predicate_mouse_in_region: function(e) {
-        var rx = e.element_x - this.x;
-        var ry = e.element_y - this.y + this.height;
-        var result = rx > 0 && ry > 0 && rx < this.width && ry < this.height;
-        log(LOG_LEVEL_DEBUG, e.element_x, e.element_y, this.label, "rx:", rx, "ry:", ry, result);
+        var rx = e.element_x - this.properties.x;
+        var ry = e.element_y - this.properties.y + this.properties.height;
+        var result = rx > 0 && ry > 0 && rx < this.properties.width && ry < this.properties.height;
+        log(LOG_LEVEL_DEBUG, e.element_x, e.element_y, this.properties.label, "rx:", rx, "ry:", ry, result);
         return result;
     },
-    entry_completed: function(e) {
-        this.message = "complete";
-        this.entry_opacity = 1.0;
+    entry_completed: function(e, rootView) {
+        this.properties.message = "complete";
+        this.properties.entry_opacity = 1.0;
         this.container.clearFocus();
-        if(typeof this.onCompleteFn !== 'undefined') {
-            this.onCompleteFn(this, rootView);
+        if(typeof this.properties.entryCompleted !== 'undefined') {
+            this.properties.entryCompleted(this, rootView);
         }
     },
     draw: function($el) {
         var s = Snap($el[0]);
-        s.text(this.x,this.y, this.label + ": ");
-        s.text(this.x + 140, this.y, this.entry_text).attr({opacity: this.entry_opacity, "font-family": "monospace"});
-        s.text(this.x + 300, this.y, this.message);
+        s.text(this.properties.x,this.properties.y, this.properties.label + ": ");
+        s.text(this.properties.x + 140, this.properties.y, this.properties.entry_text).attr({opacity: this.properties.entry_opacity, "font-family": "monospace"});
+        s.text(this.properties.x + 300, this.properties.y, this.properties.message);
         var color = "rgb(0,0,0)";
         if(this.current_state == "textEntered") {
             var char_width = 8;
 
-            s.line(this.x + 140 + this.entry_text.length * char_width, this.y, this.x + 140 + this.entry_text.length * char_width + 7, this.y)
+            s.line(this.properties.x + 140 + this.properties.entry_text.length * char_width, this.properties.y, this.properties.x + 140 + this.properties.entry_text.length * char_width + 7, this.properties.y)
                 .attr({stroke: "rgb(0,0,0)"});
         }
-        s.rect(this.x + 130, this.y - this.height + 10, this.width - 250, this.height).attr({"fill-opacity": 0, "stroke-width": 1, stroke: color});
-    },
-    clone: function() {
-        var result = new FormEntry(this.julia, this.label, this.validFunction, this.entryTextValidFunction, this.x, this.y);
-        this.copyFsm(result);
-        this.cloneActionRequests(result);
-        result.entry_text = this.entry_text;
-        result.entry_opacity = this.entry_opacity;
-        result.message = this.message;
-        result.onCompleteFn = this.onCompleteFn;
-        result.interim_text = this.interim_text;
-        return result;
+        s.rect(this.properties.x + 130, this.properties.y - this.properties.height + 10, this.properties.width - 250, this.properties.height).attr({"fill-opacity": 0, "stroke-width": 1, stroke: color});
     },
     equals: function(other) {
         if(!this._super(other)) {
             return false;
         }
-        return this.label === other.label && this.entry_text === other.entry_text && this.interim_text === other.interim_text;
+        return this.properties.label === other.label && this.properties.entry_text === other.entry_text && this.interim_text === other.interim_text;
     }
 });
