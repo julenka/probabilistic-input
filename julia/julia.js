86abf832126834c9e46630aa9d9f13c16ff025e2
julenka <julenka@cs.cmu.edu>
2014-06-13 15:06:06 -0400

    dollar_test render all gestures overlayed

--- julia/julia.js
+++ julia/julia.js
@@ -424,7 +424,7 @@ var LOG_LEVEL_VERBOSE = 4, LOG_LEVEL_DEBUG = 3, LOG_LEVEL_INFO = 2, LOG_LEVEL_ER
 // The current logging level. Modify this to change log level
 //noinspection UnnecessaryLocalVariableJS
 var gLogLevel = LOG_LEVEL_DEBUG;
-var gTraceLog = true;
+var gTraceLog = false;
 function log(level, objs) {
     var args = Array.prototype.slice.call(arguments, 1, arguments.length);
     if(gLogLevel >= level) {
