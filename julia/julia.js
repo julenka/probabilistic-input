1b6553dff9ba4468cf02887c98d5e925f57942a5
julenka <julenka@cs.cmu.edu>
2014-07-09 18:10:12 -0400

    Implement MetaFeedback: adjust feedback to display based on which interactors are ambiguous.

--- julia/julia.js
+++ julia/julia.js
@@ -3116,6 +3116,75 @@ var MeanFeedback = View.subClass({
     }
 });
 
+var MetaFeedback = Object.subClass({
+    className: "MetaFeedback",
+    init: function(julia, props) {
+        this.julia = julia;
+        this.feedbackMap = {};
+    },
+    draw: function($el) {
+        // for every element in the feedback map
+        for(var key in this.feedbackMap) {
+            var new_julia = new Julia();
+            var feedback = this.feedbackMap[key];
+            var new_alternatives = [];
+            if(key === 'default') {
+                // 'default' specifies the default feedback type to use.
+                // If a view does not have a particular id, it uses this.
+                new_julia.rootView = julia.rootView.clone();
+                var me = this;
+                function prune(root){
+                    for(var i = 0; i < root.children.length; i++) {
+                        var child = root.children[i];
+                        if (child instanceof ContainerView) {
+                            prune(child);
+                        } else {
+                            if(child.__julia_id in me.feedbackMap) {
+                                // if the child id is in the feedback map, it is being rendered by some other feedback
+                                root.children.splice(i, 1);
+                            }
+                        }
+                    }
+                }
+                prune(new_julia.rootView);
+                for(var i = 0; i < julia.alternatives.length; i++) {
+                    var new_alternative = {};
+                    new_alternative.probability = this.julia.alternatives[i].probability;
+                    new_alternative.view = this.julia.alternatives[i].view.clone();
+                    prune(new_alternative.view);
+                    new_alternatives.push(new_alternative);
+                }
+            } else {
+                var child = julia.rootView.findViewById(key);
+                // ASSUMPTION: child is always a valid ID
+                if(typeof(child) === 'undefined') {
+                    throw("MetaFeedback cannot find view with id " + key);
+                }
+
+
+                new_julia.rootView = new ContainerView();
+                new_julia.rootView.addChildView(child);
+
+                // create a new set of alternatives that we will add to a fake julia
+                // these are all of the fields that the meta feedback wants
+                for(var i = 0; i < this.julia.alternatives.length; i++) {
+                    var alternative_view = this.julia.alternatives[i].view;
+                    var new_alternative_root = new ContainerView();
+                    var new_alternative = {};
+                    new_alternative.probability = this.julia.alternatives[i].probability;
+                    var new_child = alternative_view.findViewById(key);
+                    new_alternative_root.addChildView(new_child);
+                    new_alternative.view = new_alternative_root;
+                    new_alternatives.push(new_alternative);
+                }
+            }
+            new_julia.alternatives = new_alternatives;
+            feedback.julia = new_julia;
+            feedback.draw($el);
+        }
+    }
+});
+
 var OverlayFeedback = Object.subClass({
     className: "OverlayFeedback",
     /**
@@ -3140,7 +3209,6 @@ var OverlayFeedback = Object.subClass({
         this.feedbackType = OverlayOpacity;
         this.renderThreshold = 0.01;
         this.showOriginal = true;
-        this.feedbackMap = {};
         for (var option in props) {
             this[option] = props[option];
         }
@@ -3169,12 +3237,7 @@ var OverlayFeedback = Object.subClass({
                         result.addChildView(root);
                     }
                     dirty_vps.forEach(function(vp){
-                        if(vp.view.__julia_id in me.feedbackMap) {
-                            result.addChildView(new me.feedbackMap[vp.view.__julia_id](me.julia, vp.view, vp.probability));
-                        } else {
-                            result.addChildView(new me.feedbackType(me.julia, vp.view, vp.probability));
-                        }
-
+                        result.addChildView(new me.feedbackType(me.julia, vp.view, vp.probability));
                     });
                     return result;
                 }
@@ -3218,7 +3281,6 @@ var OverlayFeedbackBase = View.subClass({
     }
 
 });
-
 /**
  * Renders a child view with opacity according to its probability
  * uses Snap library
