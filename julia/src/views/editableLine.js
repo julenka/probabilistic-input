4b9a3cb46989d68f9a041cb612a851865456f5cf
julenka <julenka@cs.cmu.edu>
2014-08-07 12:09:38 -0400

    Lines are draggable

--- julia/src/views/editableLine.js
+++ julia/src/views/editableLine.js
@@ -2,7 +2,7 @@
  * Created by julenka on 8/6/14.
  */
 
-var EditableLine = FSMView.subClass({
+var EditableLine = DraggableShape.subClass({
     className: "EditableLine",
     init: function(julia, p1, p2, props) {
         if(!props){
@@ -17,28 +17,34 @@ var EditableLine = FSMView.subClass({
         this.initFSM();
     },
     initFSM: function() {
-        this.fsm_description = {
-            start: [
-                new MouseMoveTransition(
-                    "over",
-                    this.hitTestRegion,
-                    function(){},
-                    undefined,
-                    false
-                )
-            ],
-            over: [
+        // fsm already has start, dragging states
+        this.fsm_description.start.push(
+            new MouseMoveTransition(
+                "over",
+                this.hitTestRegion,
+                function(){},
+                undefined,
+                false
+            )
+        );
+        this.fsm_description.over = [
                 new MouseMoveTransition(
                     "start",
                     function(e) { return !this.hitTestRegion(e); },
                     function(){},
                     undefined,
                     true
-                )
-            ],
-            move_p1: [],
-            move_p2: []
-        };
+                ),
+            new MouseDownTransition(
+                "dragging",
+                this.hit_test,
+                this.drag_start,
+                undefined,
+                true)
+            ];
+        this.fsm_description.move_p1 = [];
+        this.fsm_description.move_p2 = [];
+
         var me = this;
         var new_states = ["move_p1", "move_p2"];
         new_states.forEach(function(state){
@@ -80,12 +86,50 @@ var EditableLine = FSMView.subClass({
             );
         });
     },
+    /**
+     * Override the Draggable's gesture start method, we
+     * need to record our own information
+     * @param e
+     */
+    drag_start: function(e){
+
+        this.drag_start_info.mouse_x = e.base_event.element_x;
+        this.drag_start_info.mouse_y = e.base_event.element_y;
+        this.drag_start_info.p1 = shallowCopy(this.properties.p1);
+        this.drag_start_info.p2 = shallowCopy(this.properties.p2);
+    },
+    /**
+     * Need to override the Draggable gesture progress function
+     * to update endpoints
+     * @param e
+     */
+    drag_progress: function(e) {
+        var dx = e.base_event.element_x - this.drag_start_info.mouse_x;
+        var dy = e.base_event.element_y - this.drag_start_info.mouse_y;
+        this.properties.p1.x = this.drag_start_info.p1.x + dx;
+        this.properties.p1.y = this.drag_start_info.p1.y + dy;
+        this.properties.p2.x = this.drag_start_info.p2.x + dx;
+        this.properties.p2.y = this.drag_start_info.p2.y + dy;
+    },
+    /**
+     * Override the draggable hit test function
+     * @param e
+     * @returns {boolean}
+     */
+    hit_test: function(e) {
+        return this.hitTestRegion(e);
+    },
+    /**
+     * Check if mouse event is within ctrl_pt_radius of the line
+     * @param e
+     * @returns {boolean}
+     */
     hitTestRegion: function(e) {
         if(this.hitTestControlPoint(e, {to: "move_p1"}) > 0) {
-            return true;
+            return false;
         }
         if(this.hitTestControlPoint(e, {to: "move_p2"}) > 0) {
-            return true;
+            return false;
         }
         // This uses the sylvester library, see: http://sylvester.jcoglan.com/api/vector.html
         var v1 = $V([this.properties.p1.x, this.properties.p1.y]);
@@ -137,7 +181,7 @@ var EditableLine = FSMView.subClass({
         for(var state in control_states) {
             var p = control_states[state];
             s.circle(p.x, p.y, r).attr({
-                fill: state === current_state ? "red": "white",
+                fill: current_state === "dragging" ? "green" : state === current_state ? "red": "white",
                 opacity: 0.5
             });
         }
