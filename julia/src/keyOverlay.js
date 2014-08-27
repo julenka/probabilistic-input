169e1c03aa1acb8e8d418380a58697ed7cdd8470
julenka <julenka@cs.cmu.edu>
2014-08-27 13:19:16 -0400

    Instead of showing the keyboard key that was pressed, pretend like an alternative was selected

--- julia/src/keyOverlay.js
+++ julia/src/keyOverlay.js
@@ -34,3 +34,55 @@ function addKeyOverlay() {
 
     });
 }
+
+/**
+ * When a key is pressed, fake a touch animation by pretending to click an item in the upper left hand corner
+ * $el - parent element, upper left of el will be 0,0
+ */
+function addKeyFakeTouchOverlay($el) {
+    var cursorSize = 10;
+    var snap = Snap();
+    var box_size = 87;
+    var yOffset = 30;
+    var xOffset = 9;
+    snap.attr({'id': 'key-overlay'});
+
+    // we need to add these fake touches to be relative to the $el
+    // put the cursor in a container whose position is the top, left of $el
+    var container = snap.group();
+    var container_tform = new Snap.Matrix();
+    var offset = $el.offset();
+    container_tform.translate(offset.left, offset.top);
+    container.attr({transform: container_tform});
+
+    var cursor = container.circle(0,0, cursorSize).attr({fill: '#444', opacity: 0.1});
+    cursor.attr({opacity: 0});
+
+    // Uncomment to debug layout of dots
+//    for(var i = 0; i < 3; i++) {
+//        var x = xOffset + i * box_size;
+//        var y = yOffset;
+//        container.rect(x, y, box_size, box_size).attr({stroke: "red", "stroke-width": 2, "fill-opacity": 0});
+//        container.circle(x + box_size / 2, y + box_size / 2, cursorSize).attr({fill:"red"});
+//
+//    }
+    $(document).keypress(function(e) {
+        var key = e.keyCode || e.charCode;
+        if(key >= 48 && key <= 57) {
+            // subtract one from index because we start at key 1
+            var index = key - 48 - 1;
+
+            var m_start = new Snap.Matrix();
+            m_start.translate(xOffset + box_size * (index + 0.5), box_size / 2 + yOffset);
+            m_start.scale(1);
+
+            var m_end = new Snap.Matrix();
+            m_end.translate(xOffset + box_size * (index + 0.5), box_size / 2 + yOffset);
+            m_end.scale(2);
+
+            cursor.attr({opacity: 0.8, transform: m_start});
+            cursor.animate({opacity: 0.0, transform: m_end}, 200, mina.easein);
+        }
+
+    });
+}
