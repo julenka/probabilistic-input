05e6106e70d85dc3829a49f1abf09f58ce2b8787
Julia Schwarz <julenka@cs.cmu.edu>
2014-12-01 08:57:21 -0500

    Add script to open demos for talk.

---
+++ julia/demo.sh
@@ -0,0 +1,15 @@
+j=`dirname $0`
+d=$j/demo
+open $d/button.html
+open $j/test/draggable_resizeable_test.html
+open $d/gesture.html
+sleep 0.5
+open $d/octopocus_demo.html
+sleep 0.5
+open $d/octopocus_priors.html
+sleep 0.5
+open $d/text_selection_ipad.html
+sleep 0.5
+open $d/menu.html
+sleep 0.5
+open $d/diagram_app.html
